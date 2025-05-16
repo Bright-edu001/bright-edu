fbd5acfa2cf2d56c912cf3890af402d270241b4b
Harry-0824 <prince11290824@gmail.com>
2025-05-16 14:33:32 +0800

    DEBUG

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ app.use(compression({ level: 9 }));
 app.use(express.static(path.join(__dirname, "build")));
 
 // 支援 SPA 路由
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
