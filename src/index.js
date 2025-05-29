bccf80b9b1e34698cd2c9febc0b5de19bbfba41e
Harry-0824 <prince11290824@gmail.com>
2025-05-29 15:12:34 +0800

    修正sw.js帶來的錯誤

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ import performanceMonitor from "./utils/performanceMonitor";
 // 引入關鍵 CSS
 import "./styles/critical.css";
 
+// 移除舊的 Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  }).catch(function(err) {
+    console.log('Service Worker unregistration failed: ', err);
+  });
+}
+
 // 初始化性能監控
 performanceMonitor.init();
 
