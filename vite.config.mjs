3c9b4031b81cbf0098211994d604144506f43cc2
Harry-0824 <prince11290824@gmail.com>
2026-04-17 13:51:24 +0800

    消除sass警告

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,13 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern-compiler",
+      },
+    },
+  },
   server: {
     port: 3000,
     open: true,
